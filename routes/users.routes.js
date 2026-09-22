5bacd9ab271ad2065658a0fd12ce91a958f753db
unknown <pc17@csitlab.local>
2026-09-22 15:57:19 +0800

    feature: users

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -13,11 +13,12 @@ let users = [
     }
 ]
 
-router . get ('/') (req, res) =>{
+router . get ('/') (req, res)
+
        let result = users
        if (req.query.role) {
        result = users.filter(user
-        => user.role === req. query.role)
+        = user.role === req. query.role)
     }
      res. status(200).json( {
        success: true,
@@ -28,9 +29,10 @@ router . get ('/') (req, res) =>{
                count: result. length
        }
     })
-}
+    
 
-router. get ('/: id',) (req, res) =>
+
+router. get ('/: id',) (req, res) 
     const user = users. find (
         user => user. id === 
         Number(req. params. id)
@@ -46,26 +48,6 @@ router. get ('/: id',) (req, res) =>
     })
     }
 
-    res.status(200).json(
-        {
-            success: true,
-            data: user
-        })
-
-    router.post ('/', (req, res) =>) {
-        const { name, role} = req.body
-        if (!name || !role) {
-            return
-        
-        res.status(400).json ({
-            success: false,
-            error: {
-                code: 'Bad_Request'
-                message: 'name and role are required'
-            }
-        })
-        }
-    }
 
 
 

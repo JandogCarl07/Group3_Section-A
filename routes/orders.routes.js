02ee47ed4efb71cfb351e741b8fdf24ee8135d6c
kenshinn <kenshinnencarguez@gmail.com>
2026-09-22 16:02:06 +0800

    feature: order

---
+++ routes/orders.routes.js
@@ -0,0 +1,12 @@
+const express = require ('express');
+
+const route = express.Router();
+
+let orders = [
+    {
+        id: 1,
+        product: 'Iphone 15',
+        category: 'Cellphone',
+        quantity: 1
+    }
+];
